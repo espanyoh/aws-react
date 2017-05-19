5e3c925a4f28e10143819d43aa2d018d42014186
Espanyoh Yoh <hotyoh@gmail.com>
2017-05-20 00:14:34 +0700

    Sign up and Verify code

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  region: 'us-east-1',
+  IdentityPoolId: 'us-east-1_bbAxPdC2q',
+  UserPoolId: 'us-east-1_bbAxPdC2q',
+  ClientId: '1dc38aoco3ujrf03ldr2rops7p',
+}
--- src/containers/sign_up.js
+++ src/containers/sign_up.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import { Button, Panel, Grid, Row, Col, form, FormGroup, ControlLabel, HelpBlock, FormControl } from 'react-bootstrap';
 
+import {Config, CognitoIdentityCredentials} from "aws-sdk";
+import {
+  CognitoUserPool,
+  CognitoUserAttribute,
+  CognitoUser
+} from "amazon-cognito-identity-js";
+import appConfig from "../config";
+
+Config.region = appConfig.region;
+Config.credentials = new CognitoIdentityCredentials({
+  IdentityPoolId: appConfig.IdentityPoolId
+});
+const userPool = new CognitoUserPool({
+  UserPoolId: appConfig.UserPoolId,
+  ClientId: appConfig.ClientId,
+});
+
+
 class SignUp extends Component {
     constructor(props){
         super(props);
 
-        this.state = {username:'', email:'',password:''};
+        this.state = {username:'', email:'',password:'',verifyCode:''};
         this.onUsernameChange = this.onUsernameChange.bind(this);
         this.onEmailChange = this.onEmailChange.bind(this);
         this.onPasswordChange = this.onPasswordChange.bind(this);
-        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onSignUpFormSubmit = this.onSignUpFormSubmit.bind(this);
+
+        this.onVerifyCodeChange = this.onVerifyCodeChange.bind(this);
+        this.onVerifyFormSubmit = this.onVerifyFormSubmit.bind(this);
     }
   render() {
     return (
@@ -19,7 +40,7 @@ class SignUp extends Component {
             <Row className="show-grid">
                 <Col xs={6} xsOffset={3}>
                     <Panel header="Sign Up">
-                        <form onSubmit={this.onFormSubmit}>
+                        <form onSubmit={this.onSignUpFormSubmit}>
                             <FieldGroup
                             id="formControlsText"
                             type="text"
@@ -45,6 +66,22 @@ class SignUp extends Component {
                     </Panel>  
                 </Col>
             </Row>
+             <Row className="show-grid">
+                <Col xs={6} xsOffset={3}>
+                    <Panel header="Verify">
+                        <form onSubmit={this.onVerifyFormSubmit}>
+                            <FieldGroup
+                            id="formControlsText"
+                            type="text"
+                            label="Verification Code"
+                            placeholder="Enter Verification Code"
+                            onBlur={ this.onVerifyCodeChange }
+                            />
+                            <Button type="submit" bsStyle="primary" >Verify</Button>
+                        </form>
+                    </Panel>  
+                </Col>
+            </Row>
         </Grid>        
       </div>
     );
@@ -63,10 +100,53 @@ class SignUp extends Component {
         this.setState( {password : event.target.value} );
 
     }
-    onFormSubmit(event) {
+    onVerifyCodeChange(event){
+        console.log(event.target.value);
+        this.setState( {verifyCode : event.target.value} );
+    }
+    onSignUpFormSubmit(event) {
         event.preventDefault();
-        console.log('You have clicked signUp\n'+this.state.username+'\n'+this.state.email+'\n'+this.state.password);
+        console.log('---You have clicked signUp\n'+this.state.username+'\n'+this.state.email+'\n'+this.state.password);
+        const email = this.state.email.trim();
+        const password = this.state.password.trim();
+        const username = this.state.username.trim();
+        const attributeList = [
+        new CognitoUserAttribute({
+            Name: 'email',
+            Value: email,
+            })
+        ];
+        userPool.signUp(username, password, attributeList, null, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('user name is ' + result.user.getUsername());
+        console.log('call result: ' + result);
+        });
+
         this.setState({});
+    }
+    onVerifyFormSubmit(event){
+        event.preventDefault();
+        const verifyCode = this.state.verifyCode.trim();
+        const username = this.state.username.trim();
+        console.log('---You have clicked Verify \n-------\n'+username+'\n'+verifyCode+'\n--------');
+
+        var userData = {
+            Username : username,
+            Pool : userPool
+        };
+
+        var cognitoUser = new CognitoUser(userData);
+        cognitoUser.confirmRegistration(verifyCode, true, function(err, result) {
+            if (err) {
+                alert(err);
+                return;
+            }
+            console.log('call result: ' + result);
+        });        
+
     }
   
 }
